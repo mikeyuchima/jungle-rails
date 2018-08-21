becf1d8729c61d532e740e2ef931926694acf970
Michael Surya <tomichaels@gmail.com>
2018-08-21 17:48:18 +0000

    home page testing completed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.secret_key_base = ENV["123456789"]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
